28dbb596a8ce9d00b576e58898a3c48e03d5d400
Elena Naumova <naumovaelan@gmail.com>
2022-11-29 09:37:27 +0300

    add Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => {
+  return (
+    <div className="row">
+      <div className="col-sm-3">
+        <img
+          src="https://www.seekpng.com/png/full/356-3562377_personal-user.png"
+          alt="" width={"100%"}
+        />
+      </div>
+      <div className="col-sm-9">
+        <h1>Фамилия и имя</h1>
+        <h2>Рассказ о себе</h2>
+        <p>E-mail: ivan@mail.ru</p>
+        <p>Id: 2</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam veritatis cupiditate aspernatur quas explicabo iusto eius reprehenderit eum. Illum aspernatur odio neque iure magni cumque tenetur quo incidunt facere laboriosam!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
