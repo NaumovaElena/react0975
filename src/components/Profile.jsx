e49d927a6b33d1165ad18f34dfbac6a7f2477fd5
Elena Naumova <naumovaelan@gmail.com>
2022-11-29 10:08:17 +0300

    add state.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,25 @@
+import styles from "./Profile.module.css";
 const Profile = () => {
   return (
     <div className="row">
       <div className="col-sm-3">
         <img
           src="https://www.seekpng.com/png/full/356-3562377_personal-user.png"
-          alt="" width={"100%"}
+          alt=""
+          width={"100%"}
         />
       </div>
       <div className="col-sm-9">
-        <h1>Фамилия и имя</h1>
-        <h2>Рассказ о себе</h2>
+        <h1 style={{backgroundColor: "lightblue"}}>Фамилия и имя</h1>
+        <h2 className={styles.about}>Рассказ о себе</h2>
         <p>E-mail: ivan@mail.ru</p>
         <p>Id: 2</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam veritatis cupiditate aspernatur quas explicabo iusto eius reprehenderit eum. Illum aspernatur odio neque iure magni cumque tenetur quo incidunt facere laboriosam!</p>
+        <p className={styles.paragraph}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
+          veritatis cupiditate aspernatur quas explicabo iusto eius
+          reprehenderit eum. Illum aspernatur odio neque iure magni cumque
+          tenetur quo incidunt facere laboriosam!
+        </p>
       </div>
     </div>
   );
