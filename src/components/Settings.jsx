f3ac4b380487bba2b12cd9767200cb35f007e34e
Elena Naumova <naumovaelan@gmail.com>
2022-12-01 00:19:09 +0300

    Settings useState

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,5 +1,19 @@
-const Settings = () => {
-  return <h2> Страница с настройками</h2>;
-};
+import { useState } from "react";
+// const Settings = () => {
+//   return <h2> Страница с настройками</h2>;
+// };
+function Settings() {
+  const[count, setCount] = useState(0);
+  return (
+    <div className="row">
+      <div className="col-6">Количество кликов: {count}</div>
+      <div className="col-6">
+        <button className="btn btn-warning" onClick={()=>{
+          setCount(count + 1);
+        }}>Нажимай тут</button>
+      </div>
+    </div>
+  )
+}
 
 export default Settings;
