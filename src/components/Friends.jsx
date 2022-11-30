122b83d98c1eb6333f7a1bd777138164fc32efcb
Elena Naumova <naumovaelan@gmail.com>
2022-11-30 19:30:54 +0300

    Friends to Class component

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Link } from "react-router-dom";
+
 const TableRow = (props) => {
   return (
     <tr>
@@ -12,35 +14,52 @@ const TableRow = (props) => {
   );
 };
 
-const Friends = (props) => {
-  let users = props.function();
-  let usersCount = Object.keys(users).length;
-  let userRow = [];
+class Friends extends React.Component {
 
-  for (let i = 0; i < usersCount; i++) {
-    userRow.push(
-      <TableRow
-        id={users[i].id}
-        key={i}
-        index={i}
-        name={users[i].name}
-        lastname={users[i].lastname}
-      />
-    );
-  }
+  constructor(props) {
+  super(props);
+  this.state = {userRow: []};
+}
 
-  return (
-    <table className="table table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Имя и Фамилия</th>
-          {/* <th scope="col">Фамилия</th> */}
-        </tr>
-      </thead>
-      <tbody>{userRow}</tbody>
-    </table>
-  );
-};
+componentDidMount() {
+    // let users = props.function();
+    // let users = this.props.function();
+    this.props.function().then((users)=> {
+      // let usersCount = Object.keys(users).length;
+      let usersCount = users.length;
+      let userRow = [];
+
+      for (let i = 0; i < usersCount; i++) {
+        userRow.push(
+          <TableRow
+            id={users[i].id}
+            key={i}
+            index={i}
+            name={users[i].name}
+            lastname={users[i].lastname}
+          />
+        );
+      }
+      // this.setState({userRow: userRow});
+    });    
+}
+
+  render() {
+      return (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Имя и Фамилия</th>
+            </tr>
+          </thead>
+          {/* <tbody>{userRow}</tbody> */}
+          <tbody></tbody>
+          {/* <tbody>{this.state.userRow}</tbody> */}
+        </table>
+      );
+  }  
+
+}
 
 export default Friends;
--- src/state.jsx
+++ src/state.jsx
@@ -7,23 +7,28 @@ const user = {
   avatar: "https://www.seekpng.com/png/full/356-3562377_personal-user.png",
 };
 
-const users = {
-  0: { name: "Валерия", lastname: "Енотина", id: 4 },
-  1: { name: "Ипатий", lastname: "Яблочков", id: 9 },
-  2: { name: "Серж", lastname: "Петров", id: 7 },
-  3: { name: "Константин", lastname: "Тетерин", id: 2 },
-  4: { name: "Владислав", lastname: "Енотин", id: 22 },
-  5: { name: "Евгения", lastname: "Петрова", id: 14 },
-};
+// let users = {};
+// const users = {
+//   0: { name: "Валерия", lastname: "Енотина", id: 4 },
+//   1: { name: "Ипатий", lastname: "Яблочков", id: 9 },
+//   2: { name: "Серж", lastname: "Петров", id: 7 },
+//   3: { name: "Константин", lastname: "Тетерин", id: 2 },
+//   4: { name: "Владислав", lastname: "Енотин", id: 22 },
+//   5: { name: "Евгения", lastname: "Петрова", id: 14 },
+// };
 
-export function getUser(userId) {
-  for (let i = 0; i < Object.keys(users).length; i++) {
-    if (users[i].id == userId) return users[i];
-  }
+
+export function getUser() {
+//export function getUser(userId) {
+  // for (let i = 0; i < Object.keys(users).length; i++) {
+  //   if (users[i].id == userId) return users[i];
+  // }
   return user;
 }
 
 
-export function getUsers() {
+export async function getUsers() {
+  let response = await fetch("http://aroma.viccora.p-host.in/getUsers");
+  let users = await response.json();
   return users;
 }
