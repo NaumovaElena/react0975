22b1688517ac57bc5dad9ea797a0be7705a5cf18
Elena Naumova <naumovaelan@gmail.com>
2022-12-01 10:41:43 +0300

    add Tailwind, Shop (T-shorts added)

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,1 +1,95 @@
+// export default function App() {
+//   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+// }
 
+const products = [
+  {
+    id: 1,
+    name: "Черная майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "350 руб.",
+    color: "Черный",
+  },
+  {
+    id: 2,
+    name: "Серая майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "350 руб.",
+    color: "Серый",
+  },
+
+  {
+    id: 3,
+    name: "Бежевая майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "450 руб.",
+    color: "Бежевый",
+  },
+
+  {
+    id: 4,
+    name: "Интересная майка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "450 руб.",
+    color: "Бежевый",
+  },
+  {
+    id: 5,
+    name: "Супер ручка",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "4 500 руб.",
+    color: "Черный",
+  },
+  // More products...
+];
+
+export default function Shop() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-4xl font-bold tracking-tight text-gray-900">Лучшие предложения</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-red-700">
+                    <a href={product.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
